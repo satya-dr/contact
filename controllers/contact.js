d7eb4bedec1e74b90f3d6dd6771e32b0049633e3
Satyajit Roy <satyajitroy19599@gmail.com>
2025-12-14 14:12:07 +0530

    Add favourite

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,34 +1,46 @@
 const Contact = require('../models/contact');
 const Favourites = require('../models/favourites');
 
-
-const getAddContact = (req, res, next) => {
+/* ===============================
+   ADD CONTACT
+================================ */
+const getAddContact = (req, res) => {
   res.render('admin/add-contact', {
-    pageTitle: "Add Contact"
+    pageTitle: "Add Contact",
+    activePage: 'add'
   });
 };
 
-const postAddContact = (req, res, next) => {
-  const { name,phone, email, dob, address } = req.body;
-
+const postAddContact = (req, res) => {
+  const { name, phone, email, dob, address } = req.body;
   const contact = new Contact(name, phone, email, dob, address);
   contact.save();
 
   res.render('shop/contact-added', {
-    pageTitle: "Added Successfully"
+    pageTitle: "Added Successfully",
+    activePage: 'add'
   });
 };
 
-const getContact = (req, res, next) => {
+/* ===============================
+   ALL CONTACTS
+================================ */
+const getContact = (req, res) => {
   Contact.fetchAll((contacts) => {
-    const cleanContacts = contacts.filter(c => c); // ✅ remove null
+    const cleanContacts = contacts.filter(c => c);
+
     res.render('shop/user', {
       contacts: cleanContacts,
-      pageTitle: "My Contacts"
+      pageTitle: "My Contacts",
+      activePage: 'contacts'
     });
   });
 };
-const getContactDetails = (req, res, next) => {
+
+/* ===============================
+   CONTACT DETAILS
+================================ */
+const getContactDetails = (req, res) => {
   const contactId = req.params.contactId;
 
   Contact.findById(contactId, (contact) => {
@@ -38,38 +50,38 @@ const getContactDetails = (req, res, next) => {
 
     res.render('shop/contact-details', {
       contact: contact,
-      pageTitle: contact.name
+      pageTitle: contact.name,
+      activePage: 'contacts'
     });
   });
 };
 
-
-const postFavourite = (req, res, next) => {
+/* ===============================
+   FAVOURITES
+================================ */
+const postFavourite = (req, res) => {
   const contactId = req.body.contactId;
-  Favourites.addFavourite(contactId);   // ✅ এখানে add হবে
-  res.redirect('/favourites',{
-      pageTitle: "My Favourite"}
-    );
+  if (contactId) {
+    Favourites.addFavourite(contactId);
+  }
+  res.redirect('/favourites');
 };
 
-const getFavourite = (req, res, next) => {
-
-  Favourites.getFavourites((favourites)=>{
-    Contact.fetchAll((savedContacts)=>{
+const getFavourite = (req, res) => {
+  Favourites.getFavourites((favourites) => {
+    Contact.fetchAll((savedContacts) => {
       const favouritesWithDetails = favourites
-  .map(contactId =>
-    savedContacts.find(contact => contact && contact.id === contactId)
-  )
-  .filter(Boolean);
-      res.render('shop/favourite-list', {
-        favourites:favouritesWithDetails,
-      pageTitle: "My Favourite"
-    });
-    })
-  })
-  
-};
+        .map(id => savedContacts.find(c => c && c.id === id))
+        .filter(Boolean);
 
+      res.render('shop/favourite-list', {
+        favourites: favouritesWithDetails,
+        pageTitle: "My Favourite",
+        activePage: 'favourites'
+      });
+    });
+  });
+};
 
 module.exports = {
   getAddContact,
